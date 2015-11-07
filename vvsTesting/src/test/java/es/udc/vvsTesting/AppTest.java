42ea0b85eb5a4506c2460bfba19fa750a4ad229a
jorge.garcia <jorge.garcia@udc.es>
2015-11-07 21:46:50 +0100

    test para Anuncio

--- vvsTesting/src/test/java/es/udc/vvsTesting/AppTest.java
+++ vvsTesting/src/test/java/es/udc/vvsTesting/AppTest.java
@@ -1,5 +1,9 @@
 package es.udc.vvsTesting;
 
+import java.util.List;
+
+import es.udc.vvsTesting.content.Anuncio;
+import es.udc.vvsTesting.content.Content;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -34,5 +38,33 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+    public void testAnuncio(){
+    	//test atributos anuncio devuelven los que deben
+    	//Creamos anuncios
+    	Anuncio anuncio1 = new Anuncio();
+    	Anuncio anuncio2 = new Anuncio();
+    	//Comprobamos que el titulo es PUBLICIDAD
+    	assertEquals(anuncio1.obtenerTitulo(),"PUBLICIDAD");
+    	//Comprobamos que la duracion es 5
+    	assertEquals(anuncio1.obtenerDuracion(),5);
+    	//Comprobamos que al obtener la lista de Reproduccion se obtenga una lista con un elemento el anuncio
+    	List<Content> lista = anuncio1.obtenerListaReproduccion();
+    	assertEquals(lista.size(),1);
+    	for(int i =0;i<lista.size();i++){
+    		assertEquals(lista.get(i),anuncio1);
+    	}
+    	//Comprobamos que al buscar por String se devuelva una lista con un elemento o ninguno
+    	lista=null;
+    	//Comprobamos que devuelve una lista vacia si no se pasa un String con el titulo exacto
+    	lista = anuncio1.buscar("PUBLI");
+    	assertEquals(lista.size(),0);
+    	//Comprobamos que devuelve una lista con un elemento si se pasa un String con el titulo exacto
+    	lista=null;
+    	lista=anuncio1.buscar("PUBLICIDAD");
+    	assertEquals(lista.size(),1);
+    	for(int i =0;i<lista.size();i++){
+    		assertEquals(lista.get(i),anuncio1);
+    	}
     }
 }
