0078477ac45b04fee29ad701902e6fbb06240caa
Diego Campelo <diego.cmpl@gmail.com>
2015-11-12 17:11:15 +0100

    Fixed non-used var sevidorRespaldo

--- src/main/java/es/udc/vvsTesting/server/ServidorSimple.java
+++ src/main/java/es/udc/vvsTesting/server/ServidorSimple.java
@@ -1,9 +1,7 @@
 package es.udc.vvsTesting.server;
 
 public class ServidorSimple extends Servidor {
-
-	public Server servidorRespaldo;
-
+	
 	public ServidorSimple(String nombre, String tokenAdmin) {
 		super(nombre, tokenAdmin);
 	}
