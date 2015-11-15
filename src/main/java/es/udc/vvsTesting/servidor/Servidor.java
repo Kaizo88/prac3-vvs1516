0dd8c995034ea44f4cb44239ba7b8df30ff96e16
jorge.garcia <jorge.garcia@udc.es>
2015-11-15 16:30:25 +0100

    Added coments to generate javadoc

--- src/main/java/es/udc/vvsTesting/servidor/Servidor.java
+++ src/main/java/es/udc/vvsTesting/servidor/Servidor.java
@@ -8,19 +8,75 @@ import es.udc.vvsTesting.utils.InsufficientPermissionsException;
 import es.udc.vvsTesting.utils.UnexistingTokenException;
 
 public interface Servidor {
-
+	/**
+	 * Metodo que obtiene el nombre del servidor
+	 * 
+	 * @return String nombre del servidor
+	 * */
 	String obtenerNombre();
 
+	/**
+	 * Metodo que genera un nuevo token en el servidor
+	 * 
+	 * @return String nuevo token
+	 * */
 	String alta();
 
+	/**
+	 * Metodo que elimina un token en el servidor si el token no existe
+	 * devolvera una excepcion
+	 * 
+	 * @param token
+	 *            token a eliminar
+	 * @throws UnexistingTokenException
+	 *             si no existe el token a eliminar
+	 * */
 	void baja(String token) throws UnexistingTokenException;
+
+	/**
+	 * Metodo que agrega contenido a el servidor si el token no es el token de
+	 * administrador devolvera una excepcion
+	 * 
+	 * @param content
+	 *            contenido a agregar en el servidor
+	 * @param token
+	 *            token usado para agregar contenido
+	 * @throws InsufficientPermissionsException
+	 *             si el token pasado carece de permisos
+	 */
 
 	void agregar(Contenido content, String token)
 			throws InsufficientPermissionsException;
 
+	/**
+	 * Metodo que elimina contenido del servidor si el token no tiene permisos
+	 * lanza un excepcion, si el contenido no existe en el servidor lanza una
+	 * excepcion
+	 * 
+	 * @param content
+	 *            contenido a eliminar del servidor
+	 * @param token
+	 *            token usado para eliminar contenido
+	 * @throws InsufficientPermissionsException
+	 *             si el token pasado carece de permisos
+	 * @throws ContentNotFoundException
+	 *             si el contenido no existe
+	 * */
 	void eliminar(Contenido content, String token)
 			throws InsufficientPermissionsException, ContentNotFoundException;
 
+	/**
+	 * Metodo que devuelve una lista de contenido cuyo contenido contenga la
+	 * subChain pasada, si el token pasado para buscar no existe lanza una
+	 * excepcion
+	 * 
+	 * @param subChain
+	 *            String usado para buscar
+	 * @param token
+	 *            token usado para buscar
+	 * @throws UnexistingTokenException
+	 *             si el token no existe
+	 * */
 	List<Contenido> buscar(String subChain, String token)
 			throws UnexistingTokenException;
 
