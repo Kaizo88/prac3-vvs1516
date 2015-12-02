e1c26da25ee2be96e765890eba05bf197947ccde
Diego Campelo <diego.campelo@udc.es>
2015-12-02 20:03:33 +0100

    Modded custom checkstyle. Checked style on 2 files

--- src/main/java/es/udc/vvsTesting/servidor/ServidorSimple.java
+++ src/main/java/es/udc/vvsTesting/servidor/ServidorSimple.java
@@ -1,8 +1,17 @@
 package es.udc.vvsTesting.servidor;
 
+/**
+ * Clase implementador de Servidor Simple.
+ * @author diego
+ *
+ */
 public class ServidorSimple extends ServidorImp {
-	
-	public ServidorSimple(String nombre, String tokenAdmin) {
+	/**
+	 * Constructor para el servidor simple.
+	 * @param nombre nombre servidor
+	 * @param tokenAdmin token para el admin del servidor
+	 */
+	public ServidorSimple(final String nombre, final String tokenAdmin) {
 		super(nombre, tokenAdmin);
 	}
 }
--- src/main/java/es/udc/vvsTesting/utils/ContentNotFoundException.java
+++ src/main/java/es/udc/vvsTesting/utils/ContentNotFoundException.java
@@ -1,14 +1,24 @@
 package es.udc.vvsTesting.utils;
 
 import es.udc.vvsTesting.contenido.Contenido;
-
+/**
+ * Excepcion lanzada cuando no se encuentra contenido.
+ * @author diego
+ *
+ */
 @SuppressWarnings("serial")
 public class ContentNotFoundException extends Exception {
-	Contenido content;
-
-	public ContentNotFoundException(Contenido content) {
+	/**
+	 * Contenido que se buscaba.
+	 */
+	private Contenido content;
+	/**
+	 * Constructor de la excepcion.
+	 * @param content contenido que se buscaba
+	 */
+	public ContentNotFoundException(final Contenido content) {
 		this.content = content;
-		System.out.println("Content :" + content.obtenerTitulo()
+		System.out.println("Content :" + this.content.obtenerTitulo()
 				+ " not found.");
 	}
 }
