0b89394723c1595c5424a348c04ca8c8820f7eb6
jorge.garcia <jorge.garcia@udc.es>
2015-12-12 19:26:48 +0100

    Añadido test de quickcheck para alta

--- src/test/java/es/udc/vvsTesting/quickcheck/servidor/ServidorTest.java
+++ src/test/java/es/udc/vvsTesting/quickcheck/servidor/ServidorTest.java
@@ -1,5 +1,8 @@
 package es.udc.vvsTesting.quickcheck.servidor;
 
+import static net.java.quickcheck.generator.CombinedGenerators.lists;
+import static net.java.quickcheck.generator.PrimitiveGenerators.integers;
+import static net.java.quickcheck.generator.PrimitiveGenerators.strings;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
@@ -7,11 +10,17 @@ import java.util.List;
 
 import net.java.quickcheck.Generator;
 import net.java.quickcheck.generator.PrimitiveGenerators;
+import net.java.quickcheck.generator.iterable.Iterables;
 
 import org.junit.Test;
 
+import es.udc.vvsTesting.contenido.Cancion;
+import es.udc.vvsTesting.contenido.Contenido;
+import es.udc.vvsTesting.contenido.Emisora;
 import es.udc.vvsTesting.servidor.Servidor;
 import es.udc.vvsTesting.servidor.ServidorSimple;
+import es.udc.vvsTesting.utils.InsufficientPermissionsException;
+import es.udc.vvsTesting.utils.InvalidSongsDurationException;
 
 public class ServidorTest {
 	@Test
@@ -31,5 +40,58 @@ public class ServidorTest {
 		 for(int i=0;i<100;i++){
 			 assertEquals(servidores.get(i).obtenerNombre(),lista.get(i));
 		 }
+	}
+	
+	@Test
+	public void altaServidorTest(){
+		String token=null;
+		boolean alta=false;
+		for (Servidor servidor : Iterables.toIterable(new ServidorGenerator())){
+			token= servidor.alta();
+			if(token != null){
+				alta=true;
+			}
+			assertTrue(alta);
+			alta=false;
+		}
+	    		
+	}
+	//Genera servidores
+	class ServidorGenerator implements Generator<Servidor>{
+		String tokenAdmin = "4691819800";
+		ContenidoGenerator c = new ContenidoGenerator();
+		Contenido contenido = c.next();
+		public Servidor next(){
+			Servidor servidor = new ServidorSimple(strings().next(),tokenAdmin);
+			try {
+				servidor.agregar(contenido, tokenAdmin);
+			} catch (InsufficientPermissionsException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return servidor;
+		}
+	}
+	//Genera Emisoras
+	class ContenidoGenerator implements Generator<Contenido> {
+		Generator<List<String>> lGen = lists(strings());
+
+		public Contenido next() {
+		    List<String> lista = lGen.next();
+	    	Generator<Integer> iGen = integers(1,100);
+			Emisora emisora=new Emisora(strings().next());
+			for (String any : lista) { 
+				Cancion cancion;
+				try {
+					cancion = new Cancion(any, iGen.next());
+					emisora.agregar(cancion, null);
+				} catch (InvalidSongsDurationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+							
+			} 	
+			return emisora;		
+		}	
 	}
 }
